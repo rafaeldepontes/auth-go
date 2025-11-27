8fe8a7e09ca2efa3c8363252acf18b9d45e08234
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-27 13:51:43 -0300

    feat: add the Service call for Users

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/rafaeldepontes/auth-go/internal/errorhandler"
 	"github.com/rafaeldepontes/auth-go/internal/repository"
 	log "github.com/sirupsen/logrus"
 )
@@ -23,5 +25,20 @@ func NewUserService(userRepo *repository.UserRepository, logg *log.Logger) *User
 // FindAllUsers list all the users without a filter and returns each
 // one with pagination and a few datas missing for LGPD
 func (us *UserService) FindAllUsers(w http.ResponseWriter, r *http.Request) {
-	// TODO: WIP
+
+	us.Logger.Infoln("[LOG] Listing all the users in the database...")
+	var users []repository.User
+	users, err := us.userRepository.FindAllUsers()
+	if err != nil {
+		errorhandler.BadRequestErrorHandler(w, err, r.URL.Path)
+		log.Errorf("[ERROR] An error occurred: %v", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	us.Logger.Infof("[LOG] Found %v users", len(users))
+
+	//TODO: IMPLEMENT PAGINATION FOR THIS ENDPOINT
+	json.NewEncoder(w).Encode(users)
 }
