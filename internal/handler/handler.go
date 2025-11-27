203b4ce1716b2f9a0471fb6dec1aa2ed8c092f7d
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-27 11:10:01 -0300

    feat: add handler (WIP)

---
+++ internal/handler/handler.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"github.com/go-chi/chi/v5"
+	chimiddleware "github.com/go-chi/chi/v5/middleware"
+	"github.com/rafaeldepontes/auth-go/api"
+)
+
+// Handler controls the system routes based on *chi.Mux and a configuration struct.
+func Handler(r *chi.Mux, app *api.Application) {
+	r.Use(chimiddleware.StripSlashes)
+
+	// Public
+	r.Route("api/v1", func(r chi.Router) {
+		r.Get("/users", app.UserService.FindAllUsers)
+	})
+
+	// Protected
+	// WIP
+}
