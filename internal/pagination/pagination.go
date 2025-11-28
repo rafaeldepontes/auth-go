05d8f9f089b9ab3be83657155ec6a07bed31c213
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-28 17:28:55 -0300

    feat: finishing the NewPagination call

--- internal/pagination/pagination.go
+++ internal/pagination/pagination.go
@@ -2,8 +2,17 @@ package pagination
 
 import "github.com/rafaeldepontes/auth-go/internal/domain"
 
-func NewPagination[T any](data any) *domain.Pagination[T] {
+func NewPagination[T any](data []T, currentPage, totalRecords, size uint) *domain.Pagination[T] {
+	totalPages := totalRecords / size
+	if totalPages <= 0 {
+		totalPages = 1
+	}
+
 	return &domain.Pagination[T]{
-		// Data: data,
+		Data:         data,
+		CurrentPage:  currentPage,
+		TotalPages:   totalPages,
+		TotalRecords: totalRecords,
+		Size:         size,
 	}
 }
