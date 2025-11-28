8dfdf558a0d891dafc51689296247cdafae5b663
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-28 17:28:33 -0300

    fix: adjusting the Data field to accept a slice of T

--- internal/domain/pagination.go
+++ internal/domain/pagination.go
@@ -1,7 +1,7 @@
 package domain
 
 type Pagination[T any] struct {
-	Data         T    `json:"data"`
+	Data         []T  `json:"data"`
 	CurrentPage  uint `json:"currentPage"`
 	TotalPages   uint `json:"totalPages"`
 	TotalRecords uint `json:"totalRecords"`
