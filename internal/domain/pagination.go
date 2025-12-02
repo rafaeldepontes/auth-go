43799953db163ede5bad5a3513f2ca656ba1cb3d
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-12-02 11:18:12 -0300

    feat: adding the new cursor struct

--- internal/domain/pagination.go
+++ internal/domain/pagination.go
@@ -1,9 +1,15 @@
 package domain
 
-type Pagination[T any] struct {
+type OffSetPagination[T any] struct {
 	Data         []T  `json:"data"`
 	CurrentPage  uint `json:"currentPage"`
 	TotalPages   uint `json:"totalPages"`
 	TotalRecords uint `json:"totalRecords"`
 	Size         uint `json:"size"`
+}
+
+type CursorPagination[T any] struct {
+	Data       []T   `json:"data"`
+	Size       int   `json:"size"`
+	NextCursor int64 `json:"next_cursor"`
 }
